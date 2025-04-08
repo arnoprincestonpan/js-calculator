41a25f9cc1efa7ecb05f24113417f2943c970c67
Arno Pan <apan11@my.bcit.ca>
2025-04-08 00:57:26 -0700

    add evaluation

--- scripts.js
+++ scripts.js
@@ -26,6 +26,8 @@ let currentInput = '';
 let characterLimit = 15;
 let displayValue = '';
 
+
+// Event Listeners for number buttons
 buttonSeven.addEventListener('click', () => appendNumber('7'));
 buttonEight.addEventListener('click', () => appendNumber('8'));
 buttonNine.addEventListener('click', () => appendNumber('9'));
@@ -36,6 +38,27 @@ buttonOne.addEventListener('click', () => appendNumber('1'));
 buttonTwo.addEventListener('click', () => appendNumber('2'));
 buttonThree.addEventListener('click', () => appendNumber('3'));
 buttonZero.addEventListener('click', () => appendNumber('0'));
+
+// Event Listeners for operator buttons
+buttonPlus.addEventListener('click', () => appendNumber('+'));
+buttonMinus.addEventListener('click', () => appendNumber('-'));
+buttonMultiply.addEventListener('click', () => appendNumber('*'));
+buttonDivide.addEventListener('click', () => appendNumber('/'));
+
+buttonEqual.addEventListener('click', () => {
+  try {
+    const result = eval(currentInput);
+    displayValue = result.toString();
+    currentInput = displayValue;
+    if (displayValue.length > characterLimit) {
+      displayValue = displayValue.slice(0, characterLimit);
+    }
+    display.value = displayValue;
+  } catch (error) {
+    display.value = 'Error';
+    currentInput = '';
+  }
+});
 
 const removeLastCharacter = () => {
   if (currentInput.length > 0) {
